e55cc06adbe49ec54c5f95fc4d41e0e7225b00f5
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 13:08:03 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+     @message = Message.new
+     @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+               #↑チャットルームに紐づいたmessageインスタンスを生成する必要がある為、このような記述になる。
+               # 同時にストロングパラメータを適用したparamsをnewメソッドの引数にして生成したインスタンスに代入している。
+    @message.save
+  end
+
+private
+
+def message_params
+  params.require(:message).permit(:content).merge(user_id: current_user.id)
+                                            #↑ room_idはparamsに入っているがuserとも紐づける必要があるのでcurrent_user.idをmergeして紐づける
 end
+
+end
+
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to: user
-  belongs_to: room
+  belongs_to :user
+  belongs_to :room
+          # ↑スペース空けないとエラー発生する
 end
