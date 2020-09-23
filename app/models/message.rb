508c8d7276453d212c68449ca0fe9a0a2f52655a
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 19:16:19 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
           # ↑スペース空けないとエラー発生する
-  has_one_attached :image # active strageと1対1のアソシエーション
+  has_one_attached :image # active strageの画像と紐づけることができる。
+  #imageカラムは存在しないが、この記述をすることでuserモデルにimageカラムを作成せずとも画像と紐付けができる（有能）
   validates :content, presence: true
 end
