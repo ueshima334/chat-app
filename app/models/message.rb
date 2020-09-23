c4d6247ab0dc59774cda23a74cef28b714ad25da
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 19:25:08 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,11 @@ class Message < ApplicationRecord
           # ↑スペース空けないとエラー発生する
   has_one_attached :image # active strageの画像と紐づけることができる。
   #imageカラムは存在しないが、この記述をすることでuserモデルにimageカラムを作成せずとも画像と紐付けができる（有能）
-  validates :content, presence: true
+  validates :content, presence: true、unless: :was_attached?
+                                      #unless 以下の条件式に当てはまらない場合、バリデーションを適用する。
+  #was_attached?は、imageが紐づいているか？(画像が添付されているか？)を確認しているので、添付されていない場合のみ、「テキストの空欄があるとデータを保存できない」というバリデーションが適用される。                                    
+  def was_attached?
+    self.image.attached?
+  end
+
 end
