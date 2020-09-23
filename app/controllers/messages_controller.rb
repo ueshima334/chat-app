8e262841ac7a368f98ef4d20a03a8a7344d4a855
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 16:39:54 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,21 @@ class MessagesController < ApplicationController
   def index
      @message = Message.new
      @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-               #↑チャットルームに紐づいたmessageインスタンスを生成する必要がある為、このような記述になる。
+               #↑チャットルームに紐づいたmessageインスタンスを生成する必要がある為、このような記述になる。@roomは一行上で定義しているやつ
                # 同時にストロングパラメータを適用したparamsをnewメソッドの引数にして生成したインスタンスに代入している。
-    @message.save
+    if @message.save
+        redirect_to room_messages_path(@room)
+      else
+       @messages = @room.messages.includes(:user)
+       #renderでindexビューに飛ばす際に、投稿に失敗した@messagesの情報を保持することができるが、@messagesが定義されてないとエラーとなる為、indexコントローラで定義している内容をここでも記述している。
+       render :index
+    end
   end
 
 private
