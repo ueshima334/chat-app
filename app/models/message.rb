a49f1223809556b137ed9b90f94a86aeea2b4797
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 18:56:31 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
           # ↑スペース空けないとエラー発生する
+  has_one_attached :image # active strageと1対1のアソシエーション
   validates :content, presence: true
 end
