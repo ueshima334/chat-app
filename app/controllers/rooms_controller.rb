b2c5795f8b208e72de142b3a02600fcc02e45c4a
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-23 11:33:38 +0900

    プルダウンの実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,10 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    #params={"room"=>{"name"=>"ルーム名", "user_ids"=>["1", "2"]}, "commit"=>"Create Room", "controller"=>"rooms", "action"=>"create"}} 
+                                          #↑ この形にして昼間テーブルに保存するようにしている。
+              #↑ select要素でキーがroom[user_ids][]でoptionキーのvalue(値)がuser.idだと、paramsは↑のようになる。キーがroom[user][]でも形は一緒
+  end
 end
