a22af88acf5d81162ca9ffa89497a339dc1fc060
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-09-24 11:06:47 +0900

    編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,8 @@ class Message < ApplicationRecord
           # ↑スペース空けないとエラー発生する
   has_one_attached :image # active strageの画像と紐づけることができる。
   #imageカラムは存在しないが、この記述をすることでuserモデルにimageカラムを作成せずとも画像と紐付けができる（有能）
-  validates :content, presence: true、unless: :was_attached?
-                                      #unless 以下の条件式に当てはまらない場合、バリデーションを適用する。
+  validates :content, presence: true,unless: :was_attached?
+                                        #unless 以下の条件式に当てはまらない場合、バリデーションを適用する。
                                       #画像が添付されているか？の検証の結果が否の場合、「テキストの空欄があるとデータを保存できない」というバリデーションが適用される。                                    
   def was_attached?
     self.image.attached?
